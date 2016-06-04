c3fdab8ff25e0b239333351a692732064859a493
Kyle De'Vir <kyle.devir@mykolab.com>
2016-06-04 20:28:12 +1000

    Translate function - removed redundant 1.0f

--- HandmadeMath.h
+++ HandmadeMath.h
@@ -792,7 +792,6 @@ HMM_Translate(hmm_vec3 Translation)
     Result.Elements[0][3] = Translation.X;
     Result.Elements[1][3] = Translation.Y;
     Result.Elements[2][3] = Translation.Z;
-    Result.Elements[3][3] = 1.0f;
 
     return (Result);
 }
