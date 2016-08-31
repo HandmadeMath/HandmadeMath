fcc3052ed4370c56b7e7e0222a22fb00a2d4d5f3
Matt Mascarenhas <matt@handmadedev.org>
2016-08-31 02:37:37 +0100

    Make the PowerF() tests more forgiving

--- test/hmm_test.cpp
+++ test/hmm_test.cpp
@@ -60,10 +60,10 @@ TEST(ScalarMath, Power)
 TEST(ScalarMath, PowerF)
 {
     EXPECT_FLOAT_EQ(HMM_PowerF(2.0f, 0), 1.0f);
-    EXPECT_FLOAT_EQ(HMM_PowerF(2.0f, 4.1), 17.148376f);
-    EXPECT_FLOAT_EQ(HMM_PowerF(-2.0f, 4.1), -17.148376f);
-    EXPECT_FLOAT_EQ(HMM_PowerF(2.0f, -2.5), 0.176777f);
-    EXPECT_FLOAT_EQ(HMM_PowerF(-2.0f, -2.5), -0.176777f);
+    EXPECT_NEAR(HMM_PowerF(2.0f, 4.1), 17.148376f, 0.0001f);
+    EXPECT_NEAR(HMM_PowerF(-2.0f, 4.1), -17.148376f, 0.0001f);
+    EXPECT_NEAR(HMM_PowerF(2.0f, -2.5), 0.176777f, 0.0001f);
+    EXPECT_NEAR(HMM_PowerF(-2.0f, -2.5), -0.176777f, 0.0001f);
 }
 
 TEST(ScalarMath, Lerp)
