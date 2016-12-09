284e917bbbd9d785058e7939c252c67eed3cb6a5
Trinton Bullard <trintonbullard@gmail.com>
2016-12-08 22:47:41 -0700

    Cleaned HandmadeMath.cpp

--- test/HandmadeMath.cpp
+++ test/HandmadeMath.cpp
@@ -5,22 +5,3 @@
 #define GB_MATH_IMPLEMENTATION
 #include "../HandmadeMath.h"
 #include "../gb_math.h"
-
-int main() {
-
-	hmm_quaternion q1 = { 1,0,0,0 };
-	hmm_quaternion q2 = { 0,1,0,0 };
-
-	gbQuat q3 = { 1,0,0,0 };
-	gbQuat q4 = { 0,1,0,0 };
-	gbQuat result2 = { 0 };
-
-	float time = 0.5f;
-
-	hmm_quaternion Result = HMM_Slerp(q1, q2, time);
-	gb_quat_slerp(&result2, q3, q4, time);
-
-
-
-return 0;
-}
