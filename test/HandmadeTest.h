7d96bec8e1faf8ed9e124bddf79f2a70546f1aeb
Ben Visness <bvisness@gmail.com>
2018-02-18 14:17:04 -0600

    Make HandmadeTest friendlier to existing codebases

--- test/HandmadeTest.h
+++ test/HandmadeTest.h
@@ -1,3 +1,32 @@
+/*
+  HandmadeTest.h
+
+  This is Handmade Math's test framework. It is fully compatible with both C
+  and C++, although it requires some compiler-specific features.
+
+  The basic way of creating a test is using the TEST macro, which registers a
+  single test to be run:
+
+     TEST(MyCategory, MyTestName) {
+        // test code, including asserts/expects
+     }
+
+  The main function of your test code should then call hmt_run_all_tests and
+  return the result:
+
+     int main() {
+        return hmt_run_all_tests();
+     }
+
+  =============================================================================
+
+  If Handmade Test's macros are conflicting with existing macros in your
+  project, you may define HMT_SAFE_MACROS before you include HandmadeTest.h.
+  You may then prefix each macro with HMT_. For example, you may use HMT_TEST
+  instead of TEST and HMT_EXPECT_TRUE instead of EXPECT_TRUE.
+
+ */
+
 #ifndef HANDMADETEST_H
 #define HANDMADETEST_H
 
@@ -7,11 +36,11 @@
 
 #include "initializer.h"
 
-#define RESET "\033[0m"
-#define RED   "\033[31m"
-#define GREEN "\033[32m"
+#define HMT_RESET "\033[0m"
+#define HMT_RED   "\033[31m"
+#define HMT_GREEN "\033[32m"
 
-#define INITIAL_ARRAY_SIZE 1024
+#define HMT_INITIAL_ARRAY_SIZE 1024
 
 typedef struct hmt_testresult_struct {
     int count_cases;
@@ -32,34 +61,22 @@ typedef struct hmt_category_struct {
     hmt_test* tests;
 } hmt_category;
 
-int num_categories = 0;
-int category_capacity = INITIAL_ARRAY_SIZE;
+int hmt_num_categories = 0;
+int hmt_category_capacity = HMT_INITIAL_ARRAY_SIZE;
 hmt_category* categories = 0;
 
-void print_category(hmt_category c) {
-    printf("Name: %s\n", c.name);
-    printf("Number of tests: %d\n", c.num_tests);
-    printf("Tests capacity: %d\n", c.tests_capacity);
-    printf("Tests: %p\n", c.tests);
-}
-
-void print_test(hmt_test t) {
-    printf("Name: %s\n", t.name);
-    printf("Func: %p\n", t.func);
-}
-
-hmt_category new_category(const char* name) {
+hmt_category _hmt_new_category(const char* name) {
     hmt_category cat = {
         .name = name,
         .num_tests = 0,
-        .tests_capacity = INITIAL_ARRAY_SIZE,
-        .tests = (hmt_test*) malloc(INITIAL_ARRAY_SIZE * sizeof(hmt_test))
+        .tests_capacity = HMT_INITIAL_ARRAY_SIZE,
+        .tests = (hmt_test*) malloc(HMT_INITIAL_ARRAY_SIZE * sizeof(hmt_test))
     };
 
     return cat;
 }
 
-hmt_test new_test(const char* name, hmt_test_func func) {
+hmt_test _hmt_new_test(const char* name, hmt_test_func func) {
     hmt_test test = {
         .name = name,
         .func = func
@@ -71,27 +88,27 @@ hmt_test new_test(const char* name, hmt_test_func func) {
 int hmt_register_test(const char* category, const char* name, hmt_test_func func) {
     // initialize categories array if not initialized
     if (!categories) {
-        categories = (hmt_category*) malloc(category_capacity * sizeof(hmt_category));
+        categories = (hmt_category*) malloc(hmt_category_capacity * sizeof(hmt_category));
     }
 
     // Find the matching category, if possible
     int cat_index;
-    for (cat_index = 0; cat_index < num_categories; cat_index++) {
+    for (cat_index = 0; cat_index < hmt_num_categories; cat_index++) {
         if (strcmp(categories[cat_index].name, category) == 0) {
             break;
         }
     }
 
     // Expand the array of categories if necessary
-    if (cat_index >= category_capacity) {
+    if (cat_index >= hmt_category_capacity) {
         // TODO: If/when we ever split HandmadeTest off into its own package,
         // we should start with a smaller initial capacity and dynamically expand.
     }
 
     // Add a new category if necessary
-    if (cat_index >= num_categories) {
-        categories[cat_index] = new_category(category);
-        num_categories++;
+    if (cat_index >= hmt_num_categories) {
+        categories[cat_index] = _hmt_new_category(category);
+        hmt_num_categories++;
     }
 
     hmt_category* cat = &categories[cat_index];
@@ -101,7 +118,7 @@ int hmt_register_test(const char* category, const char* name, hmt_test_func func
         // TODO: If/when we ever split HandmadeTest off into its own package,
         // we should start with a smaller initial capacity and dynamically expand.
     }
-    cat->tests[cat->num_tests] = new_test(name, func);
+    cat->tests[cat->num_tests] = _hmt_new_test(name, func);
     cat->num_tests++;
 
     return 0;
@@ -112,7 +129,7 @@ int hmt_run_all_tests() {
     int count_allfailedtests = 0; // failed test cases
     int count_allfailures = 0; // failed asserts
 
-    for (int i = 0; i < num_categories; i++) {
+    for (int i = 0; i < hmt_num_categories; i++) {
         hmt_category cat = categories[i];
         int count_catfailedtests = 0; 
         int count_catfailures = 0;
@@ -134,10 +151,10 @@ int hmt_run_all_tests() {
 
             if (result.count_failures > 0) {
                 count_catfailedtests++;
-                printf("\n      " RED "(%d/%d passed)" RESET, result.count_cases - result.count_failures, result.count_cases);
+                printf("\n      " HMT_RED "(%d/%d passed)" HMT_RESET, result.count_cases - result.count_failures, result.count_cases);
                 printf("\n");
             } else {
-                printf(GREEN " [PASS] (%d/%d passed) \n" RESET, result.count_cases - result.count_failures, result.count_cases);
+                printf(HMT_GREEN " [PASS] (%d/%d passed) \n" HMT_RESET, result.count_cases - result.count_failures, result.count_cases);
             }
         }
 
@@ -149,87 +166,98 @@ int hmt_run_all_tests() {
     }
 
     if (count_allfailedtests > 0) {
-        printf(RED);
+        printf(HMT_RED);
     } else {
-        printf(GREEN);
+        printf(HMT_GREEN);
     }
-    printf("\n%d/%d tests passed overall, %d failures\n" RESET, count_alltests - count_allfailedtests, count_alltests, count_allfailures);
+    printf("\n%d/%d tests passed overall, %d failures\n" HMT_RESET, count_alltests - count_allfailedtests, count_alltests, count_allfailures);
 
     printf("\n");
 
     return (count_allfailedtests > 0);
 }
 
-#define _TEST_FUNCNAME(category, name) category ## _ ## name
-#define _TEST_FUNCNAME_INIT(category, name) category ## _ ## name ## _init
+#define _HMT_TEST_FUNCNAME(category, name) category ## _ ## name
+#define _HMT_TEST_FUNCNAME_INIT(category, name) category ## _ ## name ## _init
 
-#define TEST(category, name) \
-void _TEST_FUNCNAME(category, name)(hmt_testresult* _result); \
-INITIALIZER(_TEST_FUNCNAME_INIT(category, name)) { \
-    hmt_register_test(#category, #name, _TEST_FUNCNAME(category, name)); \
+#define HMT_TEST(category, name) \
+void _HMT_TEST_FUNCNAME(category, name)(hmt_testresult* _result); \
+INITIALIZER(_HMT_TEST_FUNCNAME_INIT(category, name)) { \
+    hmt_register_test(#category, #name, _HMT_TEST_FUNCNAME(category, name)); \
 } \
-void _TEST_FUNCNAME(category, name)(hmt_testresult* _result)
+void _HMT_TEST_FUNCNAME(category, name)(hmt_testresult* _result)
 
-#define CASE_START() \
+#define _HMT_CASE_START() \
     _result->count_cases++;
 
-#define CASE_FAIL() \
+#define _HMT_CASE_FAIL() \
     _result->count_failures++; \
-    printf("\n      - " RED "[FAIL] (%d) " RESET, __LINE__);
+    printf("\n      - " HMT_RED "[FAIL] (%d) " HMT_RESET, __LINE__);
 
 /*
  * Asserts and expects
  */
-#define EXPECT_TRUE(_actual) do { \
-    CASE_START(); \
+#define HMT_EXPECT_TRUE(_actual) do { \
+    _HMT_CASE_START(); \
     if (!(_actual)) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected true but got something false"); \
     } \
 } while (0)
 
-#define EXPECT_FALSE(_actual) do { \
-    CASE_START(); \
+#define HMT_EXPECT_FALSE(_actual) do { \
+    _HMT_CASE_START(); \
     if (_actual) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected false but got something true"); \
     } \
 } while (0)
 
-#define EXPECT_FLOAT_EQ(_actual, _expected) do { \
-    CASE_START(); \
+#define HMT_EXPECT_FLOAT_EQ(_actual, _expected) do { \
+    _HMT_CASE_START(); \
     float actual = (_actual); \
     float diff = actual - (_expected); \
     if (diff < -FLT_EPSILON || FLT_EPSILON < diff) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected %f, got %f", (_expected), actual); \
     } \
 } while (0)
 
-#define EXPECT_NEAR(_actual, _expected, _epsilon) do { \
-    CASE_START(); \
+#define HMT_EXPECT_NEAR(_actual, _expected, _epsilon) do { \
+    _HMT_CASE_START(); \
     float actual = (_actual); \
     float diff = actual - (_expected); \
     if (diff < -(_epsilon) || (_epsilon) < diff) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected %f, got %f", (_expected), actual); \
     } \
 } while (0)
 
-#define EXPECT_LT(_actual, _expected) do { \
-    CASE_START(); \
+#define HMT_EXPECT_LT(_actual, _expected) do { \
+    _HMT_CASE_START(); \
     if ((_actual) >= (_expected)) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected %f to be less than %f", (_actual), (_expected)); \
     } \
 } while (0)
 
-#define EXPECT_GT(_actual, _expected) do { \
-    CASE_START(); \
+#define HMT_EXPECT_GT(_actual, _expected) do { \
+    _HMT_CASE_START(); \
     if ((_actual) <= (_expected)) { \
-        CASE_FAIL(); \
+        _HMT_CASE_FAIL(); \
         printf("Expected %f to be greater than %f", (_actual), (_expected)); \
     } \
 } while (0)
+
+#ifndef HMT_SAFE_MACROS
+// Friendly defines
+#define TEST(category, name) HMT_TEST(category, name)
+#define EXPECT_TRUE(_actual) HMT_EXPECT_TRUE(_actual)
+#define EXPECT_FALSE(_actual) HMT_EXPECT_FALSE(_actual)
+#define EXPECT_FLOAT_EQ(_actual, _expected) HMT_EXPECT_FLOAT_EQ(_actual, _expected)
+#define EXPECT_NEAR(_actual, _expected, _epsilon) HMT_EXPECT_NEAR(_actual, _expected, _epsilon)
+#define EXPECT_LT(_actual, _expected) HMT_EXPECT_LT(_actual, _expected)
+#define EXPECT_GT(_actual, _expected) HMT_EXPECT_GT(_actual, _expected)
+#endif // HMT_SAFE_MACROS
 
 #endif // HANDMADETEST_H
