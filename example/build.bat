f5c8f23d62ff55c4ded6470941a2c6cdf9695820
Ben Visness <bvisness@gmail.com>
2018-07-10 21:31:13 -0500

    Update build.bat to better contain files

--- example/build.bat
+++ example/build.bat
@@ -1,13 +1,14 @@
 @echo off
 
-mkdir build
-pushd build
+mkdir build\example
+pushd build\example
 cl^
-    /I..\.. /I..\external\glew\include /I..\external\glfw-win64\include^
+    /I..\..\.. /I..\..\external\glew\include /I..\..\external\glfw-win64\include^
     /MD /EHsc^
-    ..\src\*.cpp^
+    ..\..\src\*.cpp^
     /Fehmm_example.exe^
     /link^
-    /LIBPATH:..\external\glew\lib\Release\x64 /LIBPATH:..\external\glfw-win64\lib-vc2015^
+    /LIBPATH:..\..\external\glew\lib\Release\x64 /LIBPATH:..\..\external\glfw-win64\lib-vc2015^
     opengl32.lib gdi32.lib user32.lib shell32.lib glew32.lib glfw3.lib
+copy ..\..\external\glew\bin\Release\x64\glew32.dll glew32.dll
 popd
