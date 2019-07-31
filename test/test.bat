3086f6615657675b871f2f2693bf0d9a6b1443a8
Ben Visness <bvisness@gmail.com>
2019-07-31 17:07:50 -0500

    Run the test EXEs as they compile

--- test/test.bat
+++ test/test.bat
@@ -13,8 +13,15 @@ if not exist "build" mkdir build
 pushd build
 
 cl /Fehmm_test_c.exe ..\HandmadeMath.c ..\hmm_test.c
+hmm_test_c
+
 cl /Fehmm_test_c_no_sse.exe /DHANDMADE_MATH_NO_SSE ..\HandmadeMath.c ..\hmm_test.c
+hmm_test_c_no_sse
+
 cl /Fehmm_test_cpp.exe ..\HandmadeMath.cpp ..\hmm_test.cpp
+hmm_test_cpp
+
 cl /Fehmm_test_cpp_no_sse.exe /DHANDMADE_MATH_NO_SSE ..\HandmadeMath.cpp ..\hmm_test.cpp
+hmm_test_cpp_no_sse
 
 popd
