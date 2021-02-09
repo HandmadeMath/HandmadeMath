38bf5f0a6533a333b61a6773ace33993f5031a08
Macoy Madson <macoy@macoy.me>
2021-02-08 18:57:25 -0800

    Fix other uses of rows which don't match C style

--- HandmadeMath.h
+++ HandmadeMath.h
@@ -2841,10 +2841,10 @@ hmm_mat4 HMM_PREFIX(Transpose)(hmm_mat4 Matrix)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
-            Result.Elements[Rows][Vectors] = Matrix.Elements[Vectors][Rows];
+            Result.Elements[Coordinates][Vectors] = Matrix.Elements[Vectors][Coordinates];
         }
     }
 
@@ -2863,10 +2863,10 @@ hmm_mat4 HMM_PREFIX(AddMat4)(hmm_mat4 Left, hmm_mat4 Right)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
-            Result.Elements[Vectors][Rows] = Left.Elements[Vectors][Rows] + Right.Elements[Vectors][Rows];
+            Result.Elements[Vectors][Coordinates] = Left.Elements[Vectors][Coordinates] + Right.Elements[Vectors][Coordinates];
         }
     }
 
@@ -2885,10 +2885,10 @@ hmm_mat4 HMM_PREFIX(SubtractMat4)(hmm_mat4 Left, hmm_mat4 Right)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
-            Result.Elements[Vectors][Rows] = Left.Elements[Vectors][Rows] - Right.Elements[Vectors][Rows];
+            Result.Elements[Vectors][Coordinates] = Left.Elements[Vectors][Coordinates] - Right.Elements[Vectors][Coordinates];
         }
     }
 
@@ -2912,17 +2912,17 @@ hmm_mat4 HMM_PREFIX(MultiplyMat4)(hmm_mat4 Left, hmm_mat4 Right)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
             float Sum = 0;
             int CurrentMatrice;
             for(CurrentMatrice = 0; CurrentMatrice < 4; ++CurrentMatrice)
             {
-                Sum += Left.Elements[CurrentMatrice][Rows] * Right.Elements[Vectors][CurrentMatrice];
+                Sum += Left.Elements[CurrentMatrice][Coordinates] * Right.Elements[Vectors][CurrentMatrice];
             }
 
-            Result.Elements[Vectors][Rows] = Sum;
+            Result.Elements[Vectors][Coordinates] = Sum;
         }
     }
 #endif
@@ -2941,10 +2941,10 @@ hmm_mat4 HMM_PREFIX(MultiplyMat4f)(hmm_mat4 Matrix, float Scalar)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
-            Result.Elements[Vectors][Rows] = Matrix.Elements[Vectors][Rows] * Scalar;
+            Result.Elements[Vectors][Coordinates] = Matrix.Elements[Vectors][Coordinates] * Scalar;
         }
     }
 
@@ -2962,16 +2962,16 @@ hmm_vec4 HMM_PREFIX(MultiplyMat4ByVec4)(hmm_mat4 Matrix, hmm_vec4 Vector)
 #ifdef HANDMADE_MATH__USE_SSE
 	Result.InternalElementsSSE = HMM_PREFIX(LinearCombineSSE)(Vector.InternalElementsSSE, Matrix);
 #else
-    int Vectors, Rows;
-    for(Rows = 0; Rows < 4; ++Rows)
+    int Vectors, Coordinates;
+    for(Coordinates = 0; Coordinates < 4; ++Coordinates)
     {
         float Sum = 0;
         for(Vectors = 0; Vectors < 4; ++Vectors)
         {
-            Sum += Matrix.Elements[Vectors][Rows] * Vector.Elements[Vectors];
+            Sum += Matrix.Elements[Vectors][Coordinates] * Vector.Elements[Vectors];
         }
 
-        Result.Elements[Rows] = Sum;
+        Result.Elements[Coordinates] = Sum;
     }
 #endif
 
@@ -2989,10 +2989,10 @@ hmm_mat4 HMM_PREFIX(DivideMat4f)(hmm_mat4 Matrix, float Scalar)
     int Vectors;
     for(Vectors = 0; Vectors < 4; ++Vectors)
     {
-        int Rows;
-        for(Rows = 0; Rows < 4; ++Rows)
+        int Coordinates;
+        for(Coordinates = 0; Coordinates < 4; ++Coordinates)
         {
-            Result.Elements[Vectors][Rows] = Matrix.Elements[Vectors][Rows] / Scalar;
+            Result.Elements[Vectors][Coordinates] = Matrix.Elements[Vectors][Coordinates] / Scalar;
         }
     }
 
