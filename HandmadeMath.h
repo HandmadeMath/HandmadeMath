cd2ab08e695e67091d026bd025e4bfdd5eb4578a
Wayde Reitsma <pootisspencerherenao@gmail.com>
2021-03-24 18:26:30 +1100

    Add brackets around HMM_MIN and HMM_MAX

--- HandmadeMath.h
+++ HandmadeMath.h
@@ -217,8 +217,8 @@ extern "C"
 #define HMM_PI32 3.14159265359f
 #define HMM_PI 3.14159265358979323846
 
-#define HMM_MIN(a, b) (a) > (b) ? (b) : (a)
-#define HMM_MAX(a, b) (a) < (b) ? (b) : (a)
+#define HMM_MIN(a, b) ((a) > (b) ? (b) : (a))
+#define HMM_MAX(a, b) ((a) < (b) ? (b) : (a))
 #define HMM_ABS(a) ((a) > 0 ? (a) : -(a))
 #define HMM_MOD(a, m) ((a) % (m)) >= 0 ? ((a) % (m)) : (((a) % (m)) + (m))
 #define HMM_SQUARE(x) ((x) * (x))
